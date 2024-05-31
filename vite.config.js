cd0896c9529afb854de90f85c18f303c28c49571
Rosa <rosalie.boulard@gmail.com>
2024-05-31 17:21:09 +0200

    WPA installation

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,30 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        theme_color: '#004e53',
+        icons: [
+          {
+            src: '/Chopping-logo.svg',
+            sizes: '144x144',
+            type: 'image/svg+xml',
+            purpose: 'any'
+          },
+          {
+            src: "/Chopping-logo.svg",
+            sizes: "192x192",
+            type: "image/svg+xml",
+            purpose: "any maskable"
+          }
+        ]
+      }
+    })
+  ],
 })
