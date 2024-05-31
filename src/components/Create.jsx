4333bce13d0a20af3eb5314b96d2e2a681aef5d3
Rosa <rosalie.boulard@gmail.com>
2024-05-31 16:05:45 +0200

    fixing some minor issues

--- src/components/Create.jsx
+++ src/components/Create.jsx
@@ -32,33 +32,30 @@ export default function Create() {
 
   return (
     <div>
-      
-        
-        {existingData ? (
-          <div className='homeErrorMsg'>
-            <h2>Vous avez déjà une liste de participants avec une cagnotte. Souhaitez-vous la détruire et recommencer ou souhaitez-vous continuer?</h2>
-            <div className='buttons'>
-              <DeletePool onPoolDelete={handlePoolDeletion} isHomePage={true} />
-              <button className="backToDrinks" onClick={backToBeer}>
-                <FontAwesomeIcon icon={faBeerMugEmpty} className="drinkIcon" />
-              </button>
-            </div>
+      {existingData ? (
+        <div className='homeErrorMsg'>
+          <h2>Vous avez déjà une liste de participants avec une cagnotte. Souhaitez-vous la détruire et recommencer ou souhaitez-vous continuer?</h2>
+          <div className='buttons'>
+            <DeletePool onPoolDelete={handlePoolDeletion} isHomePage={true} />
+            <button className="backToDrinks" onClick={backToBeer}>
+              <FontAwesomeIcon icon={faBeerMugEmpty} className="drinkIcon" />
+            </button>
           </div>
-        ) : (
-          <>
-          <div className="logoHome">
-            <img src="src/assets/img/Chopping-logo.svg" alt="logo Chopp'ing" className="ChoppinLogo" />
-            <h1>Chopping</h1>
-          </div>
-          <div className="createParticipant" key={location.key}></div>
-          <Link to='/createEvent'>
-              <button className="createBudget">
-                  <FontAwesomeIcon icon={faPlus} /> Ajouter
-              </button>
-            </Link>
-          </>
-        )}
-      </div>
-    
+        </div>
+      ) : (
+        <>
+        <div className="logoHome">
+          <img src="src/assets/img/Chopping-logo.svg" alt="logo Chopp'ing" className="ChoppinLogo" />
+          <h1>Chopping</h1>
+        </div>
+        <div className="createParticipant" key={location.key}></div>
+        <Link to='/createEvent'>
+            <button className="createBudget">
+                <FontAwesomeIcon icon={faPlus} /> Ajouter
+            </button>
+          </Link>
+        </>
+      )}
+    </div>
   );
 }
