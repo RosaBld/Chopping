20b7f0b6400804b7a3876214bb567854df5ce93c
Rosa <rosalie.boulard@gmail.com>
2024-06-07 13:09:29 +0200

    adding a delete button on the list config

--- src/components/CreateListDrink.jsx
+++ src/components/CreateListDrink.jsx
@@ -1,5 +1,5 @@
 // Libraries
-import { faPlus, faCheck, faXmark } from "@fortawesome/free-solid-svg-icons";
+import { faPlus, faCheck, faTrashCan, faXmark } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useEffect, useState } from "react"
 import ReactModal from 'react-modal';
@@ -8,7 +8,9 @@ import ReactModal from 'react-modal';
 export default function CreateListDrink() {
 
   const [listDrink, setListDrink] = useState([]);
-  const [showModal, setShowModal] = useState(false);
+  const [showModal1, setShowModal1] = useState(false);
+  const [showModal2, setShowModal2] = useState(false);
+
 
   useEffect(() => {
     const data = localStorage.getItem('listDrink');
@@ -25,32 +27,83 @@ export default function CreateListDrink() {
     const currentDrinks = [...listDrink, newDrink];
     localStorage.setItem('listDrink', JSON.stringify(currentDrinks));
     setListDrink(currentDrinks);
-    setShowModal(!showModal);
+    setShowModal2(!showModal2);
   };
 
-  const toggleModal = () => {
-    setShowModal(!showModal);
+  const toggleModal1 = () => {
+    setShowModal1(!showModal1);
   }
+
+  const toggleModal2 = () => {
+    setShowModal2(!showModal2);
+  }
+
+  const deleteDrink = (index) => {
+    const newDrinks = [...listDrink];
+    newDrinks.splice(index, 1);
+    setListDrink(newDrinks);
+    localStorage.setItem('listDrink', JSON.stringify(newDrinks));
+    setShowModal1(!showModal1);
+  };
 
   return (
     <div>
       <div className="listConfig">
         <h2>Votre liste de boissons personnalisée</h2>
         {listDrink.map((drink, index) => (
-          <div key={index}>
+          <div key={index} className="listDrinkConfig">
             <h3>{drink.name}</h3>
+            <button className="deleteDrink" onClick={toggleModal1}>
+              <FontAwesomeIcon icon={faTrashCan} />
+            </button>
+            <ReactModal 
+              isOpen={showModal1}
+              onRequestClose={toggleModal1}
+              contentLabel="Participant Form"
+              style={{
+                overlay: {
+                  backgroundColor: 'rgba(0, 0, 0, 0.25)',
+                  backdropFilter: 'blur(2px)',
+                },
+                content: {
+                  color: 'lightsteelblue',
+                  width: '50%',
+                  height: '35%',
+                  margin: 'auto',
+                  padding: '20px',
+                  border: '10px solid rgba(233, 233, 233, 1)',
+                  borderRadius: '25px',
+                  position: 'absolute',
+                  top: '0',
+                  marginTop: '50vw'
+                },
+              }}
+            >
+              <div>
+                <p className="deleteConfirmation">Etes-vous certain de vouloir supprimer cette boisson de votre liste?</p>
+                <div className="deleteOrNotDelete">
+                  <button className="cancel">
+                    <FontAwesomeIcon icon={faXmark} onClick={toggleModal1} />
+                  </button>
+                  <button onClick={() => deleteDrink(index)} className="validateDelete">
+                    <FontAwesomeIcon icon={faCheck} />
+                  </button>
+                </div>
+                
+              </div>
+            </ReactModal>
           </div>
         ))}
       </div>
 
       <div className="createDrinkable">
-        <button onClick={toggleModal} className="createDrink">
+        <button onClick={toggleModal2} className="createDrink">
           <FontAwesomeIcon icon={faPlus} />
         </button>
 
         <ReactModal 
-          isOpen={showModal}
-          onRequestClose={toggleModal}
+          isOpen={showModal2}
+          onRequestClose={toggleModal2}
           contentLabel="Participant Form"
           style={{
             overlay: {
@@ -73,7 +126,7 @@ export default function CreateListDrink() {
         >
           <div className="modalContent">
             <button className="closeModal">
-              <FontAwesomeIcon icon={faXmark} onClick={toggleModal} />
+              <FontAwesomeIcon icon={faXmark} onClick={toggleModal2} />
             </button>
           </div>
           <div className="formDrinks">
